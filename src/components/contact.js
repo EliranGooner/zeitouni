86055f59bc65a08b2d5907e330d44ba49dc2e17e
Eliran Zeitouni <eliran.zeitouni@gmail.com>
2020-03-20 14:44:50 +0200

    added contact form

--- src/components/contact.js
+++ src/components/contact.js
@@ -4,21 +4,12 @@ import React, {useState} from "react"
 import styles from './contact.module.scss'
 import { AccessAlarm, ThreeDRotation } from '@material-ui/icons';
 import { isWidthUp } from "@material-ui/core";
+import ContactForm from "./contact-form";
 
 
 const Contact = ({}) => {
     return <div className={styles.contact}>
-        <div>
-            <p>Ut amet esse laborum commodo sit id duis enim occaecat deserunt nisi deserunt culpa dolore. Cupidatat cillum in elit do aliquip ullamco proident quis incididunt magna anim. Culpa incididunt aliqua velit aute officia eiusmod laborum.
-
-            Do Lorem voluptate proident aute exercitation nostrud non ut s
-            Excepteur aute anim consequat fugiat sint sit adipisicing reprehenderit magna. Ex eu culpa Lorem culpa in commodo id aliqua excepteur. Proident quis dolor esse esse elit ea eu sunt consequat. Mollit id anim adipisicing reprehenderit labore est officia. Est quis qui incididunt reprehenderit amet non do. Incididunt duis consequat laborum officia non.
-
-Veniam sint non ullamco non tempor nisi nisi. Dolor sint incididunt enim ut. Fugiat magna ea proident aute est mollit amet veniam duis consequat consequat. Velit et sunt mollit reprehenderit ex eu laboris amet eiusmod laborum minim. Qui qui fugiat nisi minim proident est consectetur minim proident voluptate esse cillum. Ad adipisicing est eiusmod laborum mollit qui qui nisi commodo dolore elit ut.
-
-Culpa enim culpa laboris do non laboris consequat nostrud nulla ea voluptate. dolore mollit minim commodo officia. Consequat deserunt ea do deserunt aliqua eiusmod laborum nostrud fugiat. Id proident adipisicing pariatur commodo officia magna. Eiusmod amet sit excepteur mollit aliqua ullamco officia reprehenderit aliqua fugiat ipsum ut. Velit occaecat incididunt anim sunt consequat veniam adipisicing proident occaecat occaecat magna laboris. Ea cupidatat aliquip est sunt id.
-            </p>
-        </div>
+        <ContactForm action='https://www.flexyform.com/f/a9e4bfa16bf4fb187682e5e55d64f4e370308e89'></ContactForm>
     </div>
 };
 
