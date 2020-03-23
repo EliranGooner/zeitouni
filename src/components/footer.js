b54143e336503ce369e48ee25eaa43e2daa8a090
Eliran Zeitouni <eliran.zeitouni@gmail.com>
2020-03-23 15:59:49 +0200

    custom ezdev logo in footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,15 +1,34 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React, {useState} from "react"
-import styles from './footer.module.scss'
+import { Link } from "gatsby";
+import PropTypes from "prop-types";
+import React, {useState} from "react";
+import styles from './footer.module.scss';
 import {AwesomeButtonSocial} from 'react-awesome-button';
 import 'react-awesome-button/dist/themes/theme-red.css';
-import './awesome-button.css'
+import './awesome-button.css';
 import zeitouniLogo from '../images/logo 2.svg'
 import Fade from 'react-reveal/Fade';
-
+import Image from './image';
+import { graphql, useStaticQuery } from "gatsby"
+import Img from "gatsby-image"
+ 
 
 const Footer = ({}) => {
+  const data = useStaticQuery(graphql`
+    query {
+      placeholderImage: file(relativePath: { eq: "EZDev-Logo.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 1300, maxHeight: 750) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+  const imgStyle = {
+    height: '80px',
+    width: '500%',
+    margin: '0 13px 0 0'
+  }
     return <div>
       <div className={styles.slogan}>
         <Fade duration={2500}>
@@ -18,14 +37,12 @@ const Footer = ({}) => {
         </Fade>
       </div>
       <div className={styles.footer}>
+      <Link to='/sitemap'>מפת אתר</Link>
         <div className={styles.dev}>
           <p>Designed & developed by</p>
-          <Link to='/'>Eliran Zeitouni</Link>
+          <Link to='/'><Img fluid={data.placeholderImage.childImageSharp.fluid} style={imgStyle}/></Link>
         </div>
-        <div>
-            <Link to='/sitemap'>מפת אתר</Link>
-            <Link to='/accessibility'>הצהרת נגישות</Link>
-        </div>
+        <Link to='/accessibility'>הצהרת נגישות</Link>
       </div>
     </div>
 };
