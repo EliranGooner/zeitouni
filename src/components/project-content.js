673404dcf2304a8a6f2b06e1664f7028b06da79f
Eliran Zeitouni <eliran.zeitouni@gmail.com>
2020-03-28 13:26:24 +0300

    checking form validation in production

--- src/components/project-content.js
+++ src/components/project-content.js
@@ -5,11 +5,28 @@ import styles from './project-content.module.scss'
 import {AwesomeButton} from 'react-awesome-button';
 import 'react-awesome-button/dist/themes/theme-red.css';
 import './awesome-button.css'
-import Fade from 'react-reveal/Fade';
+import ElementFade from 'react-reveal/Fade';
 import divider from '../images/title-divider-white.png';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+import { useForm } from 'react-hook-form'
+
 
 
 const ProjectContent = ({project}) => {
+  const [open, setOpen] = useState(false);
+  const { register, errors, handleSubmit } = useForm()
+  const onSubmit = data => console.log(data)
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   let about
   let location
   let sale
@@ -25,23 +42,72 @@ const ProjectContent = ({project}) => {
     location = <p>הפרוייקט ממוקם במערב הרצליה הירוקה, בין הרחובות: אלתרמן (דרום), ז'בוטינסקי (מערב), והבריגדה היהודית (צפון).
 
     סמוך למרכז הבינתחומי, במרחק נגיעה מפארק הרצליה, קניון שבעת הכוכבים, קאנטרי קלאב בהרצליה, מתחם הספורטק והיכל אומנויות הבמה, מחכה לכם איכות חיים גבוהה עם שלל אפשרויות פנאי ובילוי במרחק הליכה. בואו ליהנות מכל היתרונות שלECO  הרצליה בסיפור ההצלחה הבא מבית גינדי זיתוני, מחברות הבנייה המובילות, המנוסות והמקצועיות בישראל.</p>
-
   }
 
 
     return <div className={styles.project}>
-        <Fade duration={3000}>
+        <ElementFade duration={3000}>
           <div className={styles.content}>
             <div className={styles.contact}>
+              <div className={styles.box}>
+                <div className={styles.header}>
+                  <h1>יצירת קשר</h1>
+                  </div>
+                  <p>למען קבלת פרטים נוספים וקביעת פגישה נא השאירו פרטים</p>
+                  <form action="https://www.flexyform.com/f/6dd8a2a22e656549c9cdd8b85074db127e86f10f" method="post" onSubmit={handleSubmit(onSubmit)}>
+                    <div className={styles.form}>
+                      <div className={styles.form_field}>
+                        <input type="text" name="fullName" placeholder='שם מלא' className={styles.input_row_one} ref={register({ required: true, maxLength: 30, pattern: /^[\u0590-\u05FF]+$/ })}/>
+                        <div className={styles.error}>
+                          {errors.fullName && 'שם לא תקין'}
+                        </div>
+                      </div>
+                      <div className={styles.form_field}>
+                        <input type="text" name="phone" placeholder="טלפון" className={styles.input_row_one} ref={register({ required: true, pattern: /^\d{10}$/ })}/>
+                        <div className={styles.error}>
+                          {errors.phone && 'מספר טלפון לא תקין'}
+                        </div>
+                      </div>
+                      <div className={styles.form_field}>
+                        <input type="text" name="email" placeholder='דוא״ל' className={styles.input_row_one} ref={register({ required: true, pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ })}/>
+                        <div className={styles.error}>
+                          {errors.email && 'דוא״ל לא תקין'}
+                        </div>
+                      </div>
+                      <div className={styles.submit}>
+                        <button type="submit" onClick={!errors ? handleOpen : ''}><AwesomeButton type="primary" className='aws-btn-contact' >שלח</AwesomeButton></button>
+                      </div>
+                     </div>
+                  </form>
+              </div>
             </div>
-            <div className={styles.text} dir="rtl">
+            <div className={styles.text}>
               <h1>על הפרויקט</h1>
               {about}
               <h1>המיקום</h1>
               {location}
             </div>
           </div>
+        </ElementFade>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={styles.modal}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <div className={styles.paper}>
+            <h2 id="transition-modal-title">Transition modal</h2>
+            <p id="transition-modal-description">react-transition-group animates me.</p>
+          </div>
         </Fade>
+      </Modal>
     </div>
 };
 
