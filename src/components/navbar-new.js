dade2b45d3a2bce8d36ba6c660eda85b0328e8cd
Eliran Zeitouni <eliran.zeitouni@gmail.com>
2020-04-15 18:41:23 +0300

    menu outline fix

--- src/components/navbar-new.js
+++ src/components/navbar-new.js
@@ -1,38 +1,37 @@
-import { Link } from "gatsby";
-import React, {useState, useEffect} from "react";
-import './navbar-new.scss';
-import { stack as Menu } from 'react-burger-menu';
-import Navbar from './navbar';
-import NavbarSmall from './navbar-small';
-import NavbarMedium from './navbar-medium';
-import sizeMe from 'react-sizeme';
+import { Link } from "gatsby"
+import React, { useState, useEffect } from "react"
+import "./navbar-new.scss"
+import { stack as Menu } from "react-burger-menu"
+import Navbar from "./navbar"
+import NavbarSmall from "./navbar-small"
+import NavbarMedium from "./navbar-medium"
+import sizeMe from "react-sizeme"
 
-
-const NavbarNew = (props) => {
-  const[path, setPath] = useState(null);
+const NavbarNew = props => {
+  const [path, setPath] = useState(null)
   useEffect(() => {
     const getCurrentPath = () => {
-      const url = typeof window !== 'undefined' && window.location.href;
-      const parts = url.split('/');
-      const finalSegment = parts.pop() || parts.pop();  
-      const pathArr = ['projects', 'about', 'avi', 'articles', 'contact'];
-      let urlPath;
+      const url = typeof window !== "undefined" && window.location.href
+      const parts = url.split("/")
+      const finalSegment = parts.pop() || parts.pop()
+      const pathArr = ["projects", "about", "avi", "articles", "contact"]
+      let urlPath
       pathArr.forEach(pathElement => {
         if (finalSegment.indexOf(pathElement) >= 0) {
-          urlPath = pathElement;
-        };
-      });
+          urlPath = pathElement
+        }
+      })
       if (urlPath === "#" || !pathArr.includes(urlPath)) {
-        setPath("item-home");
+        setPath("item-home")
       } else {
-        setPath(`item-${urlPath}`);
+        setPath(`item-${urlPath}`)
       }
-    };
-    getCurrentPath();
-  });
-  const { width } = props.size;
+    }
+    getCurrentPath()
+  })
+  const { width } = props.size
 
-  let ToRenderChildNavbar 
+  let ToRenderChildNavbar
   if (width > 1100) {
     ToRenderChildNavbar = Navbar
   } else if (1100 > width && width > 420) {
@@ -41,24 +40,56 @@ const NavbarNew = (props) => {
     ToRenderChildNavbar = NavbarSmall
   }
 
-
-  const showSettings = (event) => {
-    event.preventDefault();
+  const showSettings = event => {
+    event.preventDefault()
   }
-  return <div id="outer-container">
-    <ToRenderChildNavbar />
-    <Menu right disableAutoFocus pageWrapId={ "page-wrap" } outerContainerId={ "outer-container" }>
-      <Link to="/" className={'item-home' === path ? 'current-path' : null}>בית</Link>
-      <Link to="/projects/" className={'item-projects' === path ? 'current-path' : null}>פרויקטים</Link>
-      <Link to="/about/" className={'item-about' === path ? 'current-path' : null}>עלינו</Link>
-      <Link to="/avi/" className={'item-avi' === path ? 'current-path' : null}>אבי זיתוני</Link>
-      <Link to="/articles/" className={'item-articles' === path ? 'current-path' : null}>מהתקשורת</Link>
-      <Link to="/contact/" className={'item-contact' === path ? 'current-path' : null}>צרו קשר</Link>
-      <a onClick={ showSettings } className="menu-item--small" href=""></a>
-    </Menu>
-    <main id="page-wrap">
-    </main>
-  </div>
-};
+  return (
+    <div id="outer-container">
+      <ToRenderChildNavbar />
+      <Menu
+        right
+        disableAutoFocus
+        pageWrapId={"page-wrap"}
+        outerContainerId={"outer-container"}
+      >
+        <Link to="/" className={"item-home" === path ? "current-path" : null}>
+          בית
+        </Link>
+        <Link
+          to="/projects/"
+          className={"item-projects" === path ? "current-path" : null}
+        >
+          פרויקטים
+        </Link>
+        <Link
+          to="/about/"
+          className={"item-about" === path ? "current-path" : null}
+        >
+          עלינו
+        </Link>
+        <Link
+          to="/avi/"
+          className={"item-avi" === path ? "current-path" : null}
+        >
+          אבי זיתוני
+        </Link>
+        <Link
+          to="/articles/"
+          className={"item-articles" === path ? "current-path" : null}
+        >
+          מהתקשורת
+        </Link>
+        <Link
+          to="/contact/"
+          className={"item-contact" === path ? "current-path" : null}
+        >
+          צרו קשר
+        </Link>
+        <a onClick={showSettings} className="menu-item--small" href=""></a>
+      </Menu>
+      <main id="page-wrap"></main>
+    </div>
+  )
+}
 
 export default sizeMe({ monitorWidth: true })(NavbarNew)
