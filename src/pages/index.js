83d826faacebdbbd98fd5a75dd94eaa9d43b3a8e
Eliran Zeitouni <eliran.zeitouni@gmail.com>
2020-05-01 17:11:04 +0300

    added loading slider

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Hero from "../components/hero"
@@ -6,8 +6,24 @@ import Carousel from "../components/carousel"
 import AboutDiv from "../components/about-div"
 import AboutAviDiv from "../components/about-avi-div"
 import { Helmet } from "react-helmet"
+import { css } from "@emotion/core"
+import SyncLoader from "react-spinners/SyncLoader"
+
+const override = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 20rem;
+  border-color: red;
+`
 
 const IndexPage = () => {
+  const [loading, setLoading] = useState(true)
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false)
+    }, 2500)
+  })
   return (
     <Layout>
       <SEO
@@ -20,6 +36,16 @@ const IndexPage = () => {
           content="nNxHHGf6iKF31lnzDKue5h00vCt38CqZJ4RPzqDK_1U"
         />
       </Helmet>
+      <div className={`home-overlay ${loading && "show"}`}>
+        <div className="sweet-loading">
+          <SyncLoader
+            css={override}
+            size={30}
+            color={"#000000"}
+            loading={loading}
+          />
+        </div>
+      </div>
       <Hero />
       <AboutDiv />
       <Carousel />
